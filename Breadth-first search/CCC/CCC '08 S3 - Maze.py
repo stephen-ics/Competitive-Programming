9ab4f1ce00ee8923accb5ea4748ff1d58bafa31c
Stephen Ni <stephenni1234@gmail.com>
2022-08-09 17:34:10 -0400

    scuffed TLE but works CCC '08 S3 Maze

--- Breadth-first search/CCC/CCC '08 S3 - Maze.py
+++ Breadth-first search/CCC/CCC '08 S3 - Maze.py
@@ -2,58 +2,29 @@ import queue
 
 n = int(input())
 
-def printMaze(maze, path=''):
-  x = 0
-  y = 0
-  pos = set()
-  for move in path:
-    if move == 'L':
-      x -= 1
-
-    elif move == 'R':
-      x += 1
-
-    elif move == 'U':
-      y -= 1
-
-    elif move == 'D':
-      y += 1
-    pos.add((x, y))
-
-  for j, row in enumerate(maze):
-    for i, col in enumerate(row):
-      if (j , i) in pos:
-        print('+', end='')
-      else:
-        print(col + ' ', end='')
-    print()
-        
-    
-  
-
 def valid(maze, moves):
   x = 0
   y = 0
 
   for move in moves:
     if move == 'L':
-      if not (maze[y][x] == '+' and maze[y][x] == '-'):
+      if not (maze[y][x] == '+' or maze[y][x] == '-'):
         return False
       x -= 1
     elif move == 'R':
-      if not (maze[y][x] == '+' and maze[y][x] == '-'):
+      if not (maze[y][x] == '+' or maze[y][x] == '-'):
         return False
       x += 1
     elif move == 'U':
-      if not (maze[y][x] == '+' and maze[y][x] == '|'):
+      if not (maze[y][x] == '+' or maze[y][x] == '|'):
         return False
       y -= 1
     elif move == 'D':
-      if not (maze[y][x] == '+' and maze[y][x] == '|'):
+      if not (maze[y][x] == '+' or maze[y][x] == '|'):
         return False
       y += 1
 
-    if not (0 <= x < len(maze[0]) and 0 <= j < len(maze)):
+    if not (0 <= x < len(maze[0]) and 0 <= y < len(maze)):
       return False
     elif (maze[y][x] == '*'):
       return False
@@ -74,7 +45,7 @@ def findEnd(maze, moves):
     elif move == 'D':
       y += 1
     if (x == len(maze[0])-1 and y == len(maze)-1):
-      print('Found: ' + moves)
+      print(len(moves) + 1)
       return True
   return False
 
@@ -85,15 +56,21 @@ for i in range(n):
   maze = []
   for i in range(height):
     mazeLine = input()
-    maze.append(mazeLine)
+    maze.append(list(mazeLine))
 
   nums = queue.Queue()
   nums.put('')
   add = ''
+  count = 0
 
   while not findEnd(maze, add):
     add = nums.get()
+    count += 1
     for j in ['L', 'R', 'U', 'D']:
       put = add + j
       if valid(maze, put):
         nums.put(put)
+    
+    if count > 100:
+      print('-1')
+      break
