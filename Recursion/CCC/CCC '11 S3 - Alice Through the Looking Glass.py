d413d10a5f7d085bf0baf792866a2e85919c6223
stephen-ics <stephenni1234@gmail.com>
2022-08-18 19:20:30 -0400

    alice through the looking glass up till recursion

--- Recursion/CCC/CCC '11 S3 - Alice Through the Looking Glass.py
+++ Recursion/CCC/CCC '11 S3 - Alice Through the Looking Glass.py
@@ -1,9 +1,15 @@
 
 
-def magnify():
-    pass
+def magnify(coords, base, size):
+    if base[coords[1]][coords[0]] == True:
+        return 'crystal'
+    elif count == size:
+        return 'empty'
+    else:
+        pass
 
-def findBase(n, base):
+
+def findBase(n):
     unit = 5**n
     centerRight = 0.6 # under or equal 0.6
     centerLeft = 0.4 # over 0.4
@@ -11,6 +17,7 @@ def findBase(n, base):
     right = 0.8 #under or equal 0.8
     row1 = 0.6 #over or equal to 0.6
     row2 = 0.8 #over or equal to 0.8
+
 
     for i in range(unit):
         if row1 < (i+1)/unit <= 0.8:
@@ -23,20 +30,18 @@ def findBase(n, base):
                     base[i][j] = True
     return base
 
-base = {}
-
-for i in range(25):
-    base[i] = [False]*25
-
-newBase = findBase(2, base)
-print(newBase)
 
 n = int(input())
+output = []
 
 for i in range(n):
+    count = 0
     txy = input().split(" ")
-    t = txy[0]
+    size = txy[0]
+    base = findBase(size)
     coord = (txy[1],txy[2])
+    output.append(magnify(coord, base))
+    
 
     
 
