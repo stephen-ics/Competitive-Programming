24997295f3a1cfce9957f32ffbc6bf6fc59759c9
stephen-ics <stephenni1234@gmail.com>
2022-09-08 17:06:29 -0400

    more heap time complexities

--- Data Structures/Priority Queues.py
+++ Data Structures/Priority Queues.py
@@ -1,14 +1,11 @@
-# Construction
-    # Heaps: O(n)
-
-# Appending
-    # Heaps: O(log(n))
-
-# Popping/Polling (dequeuing)
-    # Heaps: O(log(n))
-
-# Peeking
-    # Heaps: O(1)
+# Construction: Heaps: O(n)
+# Appending: Heaps: O(log(n))
+# Popping/Polling (dequeuing): Heaps: O(log(n))
+# Peeking: Heaps: O(1)
+# Removing: O(log(n))
+# Advanced Removing (with help from hash table): O(log(n))
+# Contains: O(n)
+# Contains check with help from hash table: O(1) 
 
 import heapq
 
